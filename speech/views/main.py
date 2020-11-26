a1bf259ad92d20ebec265d0457fd46d812318d0f
Jon Nguyen <jon.mail@hotmail.com>
2020-11-26 15:07:30 -0800

    clean up a little and consolidate app

--- speech/views/main.py
+++ speech/views/main.py
@@ -1,6 +1,15 @@
-from speech import app, templates
-from fastapi import Request
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import numpy as np
+from speech import templates
+from joblib import load
+
+app = FastAPI()
+
+# Tell the app where the static files are
+app.mount("/static", StaticFiles(directory=".\\speech\\static"), name="static")
+templates = Jinja2Templates(directory=".\\speech\\templates")
 
 
 @app.get("/")
